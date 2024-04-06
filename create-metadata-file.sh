924fef77a186cd0370c1baf65922d1535f342ca2
Paul Savoie <paul.savoie@signpath.io>
2024-04-06 17:18:20 +0200

    adds install step for cosign

--- create-metadata-file.sh
+++ create-metadata-file.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Install cosign"
+curl -O -L "https://github.com/sigstore/cosign/releases/latest/download/cosign-linux-amd64"
+sudo mv cosign-linux-amd64 /usr/local/bin/cosign
+sudo chmod +x /usr/local/bin/cosign
+
 echo "Extracting the repository digest identifier for the given FQN"
 export IMAGE_DIGEST=`docker inspect --format='{{index .RepoDigests 0}}' "$IMAGE_FQN:$IMAGE_TAG"`
 
