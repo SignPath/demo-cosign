3a418eab365ede3572d2c918ec2b09a10eb54563
Paul Savoie <paul.savoie@signpath.io>
2024-04-22 13:45:04 +0200

    updates to action v0.3

--- attach-signature.sh
+++ attach-signature.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-echo "Extract the detached signature"
-unzip -n signed-metadata.zip
-
 echo "Encode the signature using base64 for cosign"
-cat payload.json.sig | base64 > payload.json.base64.sig
+cat signed-metadata/payload.json.sig | base64 > payload.json.base64.sig
 
 echo "Upload the signature to the registry"
 cosign attach signature --payload payload.json --signature payload.json.base64.sig $IMAGE_DIGEST
